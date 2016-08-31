3a636a02e3992e35f4daa3ad4eb448f134029cd0
Fergusoncalv <fergusoncalv@hotmail.com>
2016-08-31 16:56:42 -0400

    Update Lab#5

--- Lab_5/Program.cs
+++ Lab_5/Program.cs
@@ -10,6 +10,34 @@ namespace Lab_5
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Welcome to the Grand Circus!");
+            Console.WriteLine("Do you want to Roll the Dice?");
+            Console.WriteLine("yes/no");
+            bool stopProgram = false;
+            if (!stopProgram)
+            {
+                string Great = Console.ReadLine();
+            }
+            Console.WriteLine("Great");
+
+            string answer = "yes";
+            while (answer == "yes")
+            {
+                Console.WriteLine("Time to Roll the Dice");
+                Console.WriteLine("Rollem");
+                Console.WriteLine("Haa");
+                Random generator = new Random();
+                int number = generator.Next(7);
+                Console.WriteLine(number);
+                Random generator2 = new Random();
+                int number2 = generator.Next(7);
+                Console.WriteLine(number2);
+                Console.WriteLine("Do you want to roll again?");
+                Console.WriteLine("yes/no");
+                answer = Console.ReadLine();
+            }
+
         }
     }
 }
